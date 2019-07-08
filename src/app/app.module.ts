2bfb9b62602add8e968d5ba7ecf79d3fb1fce2fa
Hunter Ko <hko.tw@pmgi.local>
2019-07-08 18:21:17 +0800

    'home-layout'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './component/todo/todo.component';
+import { TimerComponent } from './component/timer/timer.component';
+import { SideNavbarComponent } from './component/side-navbar/side-navbar.component';
+import { HomeComponent } from './container/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/header/header.component';
+import {MatIconModule, MatProgressSpinnerModule} from '@angular/material';
+import { MissionInputComponent } from './component/mission-input/mission-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TimerComponent,
+    SideNavbarComponent,
+    HomeComponent,
+    HeaderComponent,
+    MissionInputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/mission-input/mission-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-input',
+  templateUrl: './mission-input.component.html',
+  styleUrls: ['./mission-input.component.css']
+})
+export class MissionInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
